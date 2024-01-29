295d67ccf5fe04ba945d628ae5dfaf57aff154ca
Utkarsh Chaudhary <utkarshchaudhary12@gmail.com>
2024-01-30 01:13:13 +1100

    optimizations

--- src/app.js
+++ src/app.js
@@ -6,56 +6,31 @@ const CategoryRouter = require('./routes/category.routes');
 const PostRoutes = require('./routes/post.routes');
 const UserRoutes = require('./routes/user.routes');
 const TagRoutes = require('./routes/tag.routes');
-// const CommentRoutes = require('./routes/comment.routes');
 const AuthRoutes = require('./auth/auth.routes');
 const ImageRouter = require('./routes/image.routes');
 
-
 const cors = require('cors');
-// cors({ credentials: true, origin: true });
 const ConnectDB = require('./database');
 const session = require('express-session');
 
-
 const crypto = require('crypto');
-const { mongo, default: mongoose } = require('mongoose');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
-
-var allowedOrigins = ['http://localhost:3000', 'https://siddhi-blog.vercel.app'];
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+    // exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
 }));
 
-app.options('https://siddhi-blog.vercel.app', function (req, res, next) {
+app.options('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "accept, content-type");
     res.header("Access-Control-Max-Age", "1728000");
     return res.sendStatus(200);
 });
-
-
-
-// app.use(session({
-//     genid: function(req) {
-//         return genuuid() // use UUIDs for session IDs
-//       },
-//     secret: crypto.randomBytes(20).toString("hex"),
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {  }
-// }));
 
 function genuuid() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
@@ -64,16 +39,11 @@ function genuuid() {
 }
 
 const sess = {
-    genid: function (req) {
-        return genuuid() // use UUIDs for session IDs
-    },
+    genid: function (req) { return genuuid() },
     secret: crypto.randomBytes(20).toString("hex"),
-    cookie: {
-        maxAge: 60000,
-    },
+    cookie: { maxAge: 60000 },
     resave: false,
     saveUninitialized: true,
-    // store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
 }
 
 if (app.get('env') === 'production') {
@@ -82,9 +52,6 @@ if (app.get('env') === 'production') {
 }
 
 app.use(session(sess))
-
-
-
 
 const port = process.env.PORT || 4000;
 
@@ -99,8 +66,6 @@ app.get('/', (req, res) => {
     })
 })
 
-
-
 app.get('/session', (req, res) => {
     res.send(req.session)
 })
@@ -110,30 +75,10 @@ app.use('/api/v1/blog/posts', PostRoutes);
 app.use('/api/v1/blog/user', UserRoutes);
 app.use('/api/v1/auth', AuthRoutes);
 app.use('/api/v1/blog/tags', TagRoutes);
-// app.use('/api/v1/blog/comments', CommentRoutes());
 app.use('/api/v1/blog/images', ImageRouter);
-
-
-
-// app.options('https://siddhi-blog.vercel.app/', function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', "*");
-//     res.header('Access-Control-Allow-Methods', 'POST');
-//     res.header('Access-Control-Allow-Methods', 'GET');
-//     res.header("Access-Control-Allow-Headers", "accept, content-type");
-//     res.header("Access-Control-Max-Age", "1728000");
-//     return res.sendStatus(200);
-// });
-
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
     console.log("http://localhost:" + port);
     ConnectDB()
 })
-
-
-
-
-
-
