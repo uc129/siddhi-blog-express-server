df06388d5b59f1ecf1d0d7262019fe357aff3143
Utkarsh Chaudhary <utkarshchaudhary12@gmail.com>
2024-01-29 15:33:52 +1100

    cors

--- src/app.js
+++ src/app.js
@@ -20,10 +20,22 @@ const session = require('express-session');
 const crypto = require('crypto');
 const { mongo, default: mongoose } = require('mongoose');
 const app = express();
-app.use(cors({
-    origin: "*"
-}));
 
+
+var allowedOrigins = ['http://localhost:3000', 'https://siddhi-blog.vercel.app'];
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 
 
